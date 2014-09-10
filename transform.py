561192136fd9fe7756b12acb5f8c020730b3041a
wessel <wessel@wessel.(none)>
2014-09-10 13:23:32 +0200

    Cleaned up, and added code that makes Spanish work

--- transform.py
+++ transform.py
@@ -8,48 +8,12 @@ import skiff
 def dropletname_to_ip(name):
 
     #Authenticate with Digital Ocean
-    skiff.token('3461da98023e93f0a40e1058092314fbbd5cf057ceb1f1ae1e4d2d6d0d6e2209');    
+    s = skiff.rig('3461da98023e93f0a40e1058092314fbbd5cf057ceb1f1ae1e4d2d6d0d6e2209');    
 
-    for droplet in skiff.Droplet.all():    
+    for droplet in s.Droplet.all():    
 
         if droplet.name == name:
             return droplet.v4[1].ip_address;
-
-def timeout(func, args=(), kwargs={}, timeout_duration=3, default=None):
-    import signal
-
-    class TimeoutError(Exception):
-        pass
-
-    def handler(signum, frame):
-        raise TimeoutError()
-
-    # set the timeout handler
-    signal.signal(signal.SIGALRM, handler) 
-    signal.alarm(timeout_duration)
-    try:
-		print(1);
-		result = func(*args, **kwargs)
-		print(2);
-    except TimeoutError as exc:
-		print(3);
-		result = default
-		print(4);
-    finally:
-		print(5);
-		signal.alarm(0)
-		print(6);
-
-    return result
-
-def start_background_servers(c,lang):
-	
-	if lang == 'eng':
-		for command in ['timblserver --config=/root/fowlt/servers/timblservers/confusibles.conf --pidfile=/root/fowlt/servers/pid &',]:
-                                #'python /root/fowlt/servers/start_woprserver.py &']:
-			print('Started',command);
-			c.execute(command);
-			print('Finished',command);
 
 def stop_background_servers(c,lang):
 
@@ -68,9 +32,6 @@ def add_language(c,lang):
 	#Add language to the config file
         c.execute('python /root/tools/edit_languages.py add '+lang);
 
-	#Start related background servers
-#	start_background_servers(c,lang);
-
 def remove_language(c,lang):
 	
 	if lang == 'nld':
@@ -78,14 +39,15 @@ def remove_language(c,lang):
 	elif lang == 'eng':
 		corrector = 'fowlt';
 
-        #Stop the script
-        c.execute('pkill -f \'python /root/rabbit_receiver.py '+corrector+'\'');
+	if lang in ['nld','eng']:
+	        #Stop the script
+	        c.execute('pkill -f \'python /root/rabbit_receiver.py '+corrector+'\'');
+
+		#Stop related background servers
+		stop_background_servers(c,lang);
 
 	#Remove language from the config file
         c.execute('python /root/tools/edit_languages.py remove '+lang);
-
-	#Stop related background servers
-	stop_background_servers(c,lang);
 
 try:
 	name = sys.argv[1];
